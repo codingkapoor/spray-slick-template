428f95ac5ca3861e6f3d197274fa028f4f9bce2f
Tim Gent <tim.gent@gmail.com>
2016-07-09 08:43:55 +0100

    3rd example added using HLists

---
+++ src/main/scala/com/timmeh/openhr/openholidays/model/LargeUnnestedTableRow.scala
@@ -0,0 +1,9 @@
+package com.timmeh.openhr.openholidays.model
+
+// Let's say these are the case classes that you eventually want to map
+case class LargeUnnestedTableRow(id: Int, a: String, b: String, c: String, d: String,
+                         e: String, f: String, g: String, h: String, i: String,
+                         j: String, k: String, l: String, m: String, n: String,
+                         o: String, p: String, q: String, r: String, s: String,
+                         t: String, u: String, v: String, w: String, x: String,
+                         y: String, z: String)
